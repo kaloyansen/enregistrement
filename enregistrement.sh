3098c628b41d6cb3b4e9f881b60ac512cb2ef641
Kaloyan Krastev <kaloyansen@gmail.com>
2025-03-16 16:35:24 +0200

    record

--- enregistrement.sh
+++ enregistrement.sh
@@ -1,39 +1,37 @@
 #!/bin/sh
 
-MIDI_DEVICE="20:0"
+MIDI_DEVICE=$(aconnect -l | grep "Roland Digital Piano" | grep client | awk '{print $2}')
+MIDI_DEVICE="${MIDI_DEVICE}0"
 OUTDIR="$HOME/$(basename "$0")"
 OUTDIR=$HOME/enregistrement
 LAST_RECORD=$OUTDIR/last.mid
-mkdir $OUTDIR
+mkdir -p $OUTDIR
 STOP_NOTE=24
 
-
-echo "midi recording service started on device $MIDI_DEVICE..."
-echo "press any key to start recording"
-echo "press note $STOP_NOTE to stop"
-
+echo midi recording service started on device $MIDI_DEVICE...
 aseqdump -p "$MIDI_DEVICE" | while read -r line; do
 
     if echo "$line" | grep -q "Note on"; then
 
-        echo "starting new recording..."
+        echo -n starting a new recording...
         OUTFILE="$OUTDIR/$(date +%Y%m%d%H%M%S).mid"
         arecordmidi -p "$MIDI_DEVICE" "$OUTFILE" &
         RECORD_PID=$!
-
-        echo "recording started"
+        echo ok
+	echo press note $STOP_NOTE to stop recording
 
         while read -r line; do
+
             if echo "$line" | grep -q "Note on" && echo "$line" | grep -q "note $STOP_NOTE"; then
-                echo "stopping recording"
-                kill "$RECORD_PID"
+                echo -n stopping recording ...
+                kill $RECORD_PID
 		rm -f $LAST_RECORD
-                echo "recording saved to $OUTFILE"
+                echo recording saved to $OUTFILE
 		ln -s $OUTFILE $LAST_RECORD
+                echo press any key to start a new recording
                 break
             fi
         done
-
     fi
 done
 
