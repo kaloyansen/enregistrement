a0a4815d301d1851c3126a120250ce120ce5f5ce
Kaloyan Krastev <kaloyansen@gmail.com>
2025-03-19 11:52:15 +0200

    service

---
+++ bin/reconnect.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+SSH_USER=kalo
+SSH_HOST=192.168.0.101
+SSH_DELAY=100
+
+while true; do
+
+    now=`date`
+    echo -n $now trying to connect to $SSH_HOST ...
+    ssh -o ConnectTimeout=10 -o BatchMode=yes "$SSH_USER@$SSH_HOST" "sleep $SSH_DELAY && exit"
+    if [ $? -eq 0 ]; then
+
+        echo connection successful
+    else 
+
+	echo connection lost $?
+        sleep $SSH_DELAY
+    fi
+done
--- enregistrement.sh
+++ enregistrement.sh
@@ -7,14 +7,19 @@ OUTDIR=$HOME/enregistrement
 LAST_RECORD=$OUTDIR/last.mid
 mkdir -p $OUTDIR
 STOP_NOTE=24
+MUSIC=`awk 'BEGIN { print 2^(1/12) }'`
+FREQ=1740
 
-echo midi recording service started on device $MIDI_DEVICE...
-aseqdump -p "$MIDI_DEVICE" | while read -r line; do
+
+echo midi recording service start on device $MIDI_DEVICE...
+# aseqdump -p "$MIDI_DEVICE" | while read -r line; do
+stdbuf -oL aseqdump -p "$MIDI_DEVICE" | grep --line-buffered "Note on" | while read -r line; do
 
     if echo "$line" | grep -q "Note on"; then
 
-        echo -n starting a new recording...
-        OUTFILE="$OUTDIR/$(date +%Y%m%d%H%M%S).mid"
+        now=$(date +%Y%m%d%H%M%S)
+        echo -n $now starting a new recording...
+        OUTFILE="$OUTDIR/$now.mid"
         arecordmidi -p "$MIDI_DEVICE" "$OUTFILE" &
         RECORD_PID=$!
         echo ok
@@ -22,17 +27,26 @@ aseqdump -p "$MIDI_DEVICE" | while read -r line; do
 
         while read -r line; do
 
+            sleep 0.03
             if echo "$line" | grep -q "Note on" && echo "$line" | grep -q "note $STOP_NOTE"; then
-                echo -n stopping recording ...
+                echo -n got signal ...
                 kill $RECORD_PID
 		rm -f $LAST_RECORD
                 echo recording saved to $OUTFILE
 		ln -s $OUTFILE $LAST_RECORD
+                echo -n alert ...
+		for i in $(seq 8); do
+
+                    pitch=$((RANDOM % 13 - 6))
+		    FREQ=$(awk -v f=$FREQ -v r="$MUSIC" -v s=$pitch 'BEGIN { printf "%.0f", f * (r ^ s) }')
+                    beep -f $FREQ -l 220
+		done
                 echo standby
-		beep -f 1760 -l 500
                 break
             fi
         done
     fi
 done
+echo midi recording service stop
+
 
