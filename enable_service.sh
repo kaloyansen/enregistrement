9748db845d29479adae8b687b1877544213736b1
Kaloyan Krastev <kaloyansen@gmail.com>
2025-03-19 11:58:03 +0200

    service

--- enable_service.sh
+++ enable_service.sh
@@ -1,7 +1,17 @@
 #!/usr/bin/env sh
 
-sudo cp enregistrement.service /usr/lib/systemd/system/.
+SERVICE_SRC=/yocto/enregistrement/service
+
+sudo cp $SERVICE_SRC/enregistrement.service /usr/lib/systemd/system/.
+sudo cp $SERVICE_SRC/reconnect.service /usr/lib/systemd/system/.
 sudo systemctl daemon-reload
+
 sudo systemctl restart enregistrement
 sudo systemctl enable enregistrement
+sudo systemctl restart reconnect
+sudo systemctl enable reconnect 
+sudo systemctl status enregistrement
+sudo systemctl status enregistrement
+
+
 
